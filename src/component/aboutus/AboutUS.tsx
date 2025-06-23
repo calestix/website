f1ad76a8e609f5aefa232ff4aaaf5ff370135f46
calestix <calestixinfotech@gmail.com>
2025-06-24 00:32:12 +0530

    r1

--- src/component/aboutus/AboutUS.tsx
+++ src/component/aboutus/AboutUS.tsx
@@ -8,7 +8,7 @@ export const AboutUS = () => {
         <div className="heading">
             <h4>About Us</h4>
         </div>
-        <div className="flex pt-4 items-center mt-12">
+        <div className="lg:flex pt-4 items-center mt-12">
               <div className="basis-1/2">
               <p>At Celestix, we specialize in a designing and developing high-quality websites and applications tailored to your business needs. Whether you&apos;re an industry, school, college, e-commerce business, or e-learning platform, we provide cutting-edge digital solutions to help you grow and succeed online.</p>
               <p>Our team of experts is dedicated to delivering custom, scalable, and user-friendly solutions that enhance your online presence and drive results. From dynamic websites to feature-rich applications, we ensure seamless functionality and an outstanding user experience.</p>
@@ -18,7 +18,7 @@ export const AboutUS = () => {
               <Image 
               src={img}
               alt='img'
-              className='img-fluid'
+              className='img-fluid lg:mt-0 mt-4'
               />
               </div>
             </div>
